232b0d9c3a00e48ffb35e7ad0a0d033d99f4affc
peteroneshift <peter@oneshift.com.au>
2015-11-20 10:32:08 +1100

    add spring.net web form project

---
+++ CZBK.ItcastOA.SpringNet/UserInfoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CZBK.ItcastOA.SpringNet
+{
+    class UserInfoService:IUserInfoService
+    {
+        public string UserName { get; set; }
+        public Person Person { get; set; }
+
+        public string ShowMsg()
+        {
+            return "hello world"+UserName+"age :"+Person.Age;
+        }
+    }
+}
