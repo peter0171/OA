209c38203800578469e1d459974771aeff9d3afc
peteroneshift <peter@oneshift.com.au>
2015-11-20 12:41:09 +1100

    add log4net to log error msg.

--- CZBK.ItcastOA.WebApp/App_Start/FilterConfig.cs
+++ CZBK.ItcastOA.WebApp/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using CZBK.ItcastOA.WebApp.Models;
+using System.Web;
 using System.Web.Mvc;
 
 namespace CZBK.ItcastOA.WebApp
@@ -7,7 +8,8 @@ namespace CZBK.ItcastOA.WebApp
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new MyExceptionAttribute());
         }
     }
 }
--- CZBK.ItcastOA.WebApp/Global.asax.cs
+++ CZBK.ItcastOA.WebApp/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CZBK.ItcastOA.WebApp.Models;
+using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -16,12 +19,37 @@ namespace CZBK.ItcastOA.WebApp
     {
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure();//读取Log4Net配置信息
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            string fileLogPath = Server.MapPath("/Log/");
+            //WaitCallback
+            ThreadPool.QueueUserWorkItem((a) =>
+            {
+                while (true)
+                {
+                    if (MyExceptionAttribute.ExceptionQueue.Count > 0)
+                    {
+                        Exception ex = MyExceptionAttribute.ExceptionQueue.Dequeue();//出队
+                        //string fileName = DateTime.Now.ToString("yyyy-MM-dd")+".txt";
+                        //File.AppendAllText(fileLogPath + fileName, ex.ToString(), System.Text.Encoding.Default);
+                        ILog logger = LogManager.GetLogger("errorMsg");
+                        logger.Error(ex.ToString());
+                    }
+                    else
+                    {
+                        Thread.Sleep(3000);//如果队列中没有数据，休息避免造成CPU的空转.
+                    }
+                }
+
+
+            }, fileLogPath);
         }
     }
 }
